2a9137439afa85b6de819dcd1271d6f32b258961
admacpherson <102562791+admacpherson@users.noreply.github.com>
2025-12-30 19:15:04 -0800

    Modularize as function and accept bidirectional input
    DOT data only contains one entry per city pair so this function tries (city1, city2) then flips the order if no results are found, ensuring that the city pair data is returned

--- src/query.py
+++ src/query.py
@@ -22,18 +22,34 @@ airport_IDs = {
 }
 
 # Dataset identifier for querying DOT
-DOT_DATASET_ID = "4f3n-jbg2"
+TABLE1_DATASET_ID = "4f3n-jbg2"
 
 # Client for querying DOT
 client = Socrata("data.transportation.gov", APP_TOKEN)
 
+# Capture all flight data between two cities as an ordered pair and return as df (helper function)
+def run_city_pair_query(city1, city2, limit=500, dataset_id = TABLE1_DATASET_ID):
+    # Try with city1 then city2
+    results = client.get(TABLE1_DATASET_ID,
+                         limit=limit,
+                         citymarketid_1=city1,
+                         citymarketid_2=city2)
+    # Convert to pandas DataFrame
+    results_df = pd.DataFrame.from_records(results)
+    return results_df
+
+# Capture all flight data between two cities as an unordered pair and return as df (best for usage)
+def fetch_city_pair_data(city1, city2, limit=500, dataset_id = TABLE1_DATASET_ID):
+    # Try with city1 then city2
+    results_df = run_city_pair_query(city1, city2, limit, dataset_id)
+
+    # If city pairs are backwards, switch as parameters (DOT only has one entry per pair)
+    if results_df.shape[0] > 1:
+        return results_df
+    else:
+        results_df = run_city_pair_query(city2, city1, limit, dataset_id)
+        return results_df
+
+
 # Capture all flights between LA Metro airports and Seattle (DOT data is bidirectional)
-results = client.get(DOT_DATASET_ID,
-                         limit=100,
-                         citymarketid_1=city_IDs["LA Metro"],
-                         citymarketid_2=city_IDs["Seattle"])
-
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
-
-print(results_df)
+la_seattle_data = fetch_city_pair_data(city_IDs["Seattle"], city_IDs["LA Metro"], limit=100)
